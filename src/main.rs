e60a300e778c40b63a626c3c24377bb8f88c808f
Will Tekulve <tekulve.will@gmail.com>
2020-01-21 01:55:35 -0500

    WIP setting up sercom0 interrupts

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,65 @@ extern crate cortex_m_semihosting;
 extern crate feather_m0 as hal;
 extern crate panic_halt;
 extern crate rtfm;
+#[macro_use]
+extern crate nb;
 
-use hal::pac::{CorePeripherals, Interrupt, Peripherals};
+use hal::{
+    clock::GenericClockController,
+    pac::{CorePeripherals, Interrupt, Peripherals},
+    prelude::*,
+};
 use rs485_transport::Transport;
 
 const DEVICE_ADDRESS: u8 = 0x1;
 
+type UARTPeripheral = hal::sercom::UART0<
+    hal::sercom::Sercom0Pad3<hal::gpio::Pa11<hal::gpio::PfC>>,
+    hal::sercom::Sercom0Pad2<hal::gpio::Pa10<hal::gpio::PfC>>,
+    (),
+    (),
+>;
+
 #[rtfm::app(device = hal::pac)]
 const APP: () = {
     struct Resources {
-        core: CorePeripherals,
-        peripherals: Peripherals,
         transport: Transport,
+        uart: UARTPeripheral,
+        sercom0: hal::pac::SERCOM0,
     }
     #[init]
     fn init(_: init::Context) -> init::LateResources {
+        let mut core = CorePeripherals::take().unwrap();
+        let mut peripherals = Peripherals::take().unwrap();
+        let mut clocks = GenericClockController::with_external_32kosc(
+            peripherals.GCLK,
+            &mut peripherals.PM,
+            &mut peripherals.SYSCTRL,
+            &mut peripherals.NVMCTRL,
+        );
+        let mut pins = hal::Pins::new(peripherals.PORT);
+
+        // Enable sercom0 receive complete interrupt
+        peripherals
+            .SERCOM0
+            .usart_mut()
+            .intenset
+            .write(|w| w.rxc().set_bit());
+
+        let uart = hal::uart(
+            &mut clocks,
+            1.mhz(),
+            peripherals.SERCOM0,
+            &mut peripherals.PM,
+            pins.d0,
+            pins.d1,
+            &mut pins.port,
+        );
+
         init::LateResources {
-            core: CorePeripherals::take().unwrap(),
-            peripherals: Peripherals::take().unwrap(),
             transport: Transport::new(DEVICE_ADDRESS),
+            uart: uart,
+            sercom0: unsafe { Peripherals::steal().SERCOM0 },
         }
     }
 
@@ -33,9 +73,22 @@ const APP: () = {
         loop {}
     }
 
-    #[task(binds = SERCOM0)]
-    fn sercom0(cx: sercom0::Context) {}
-
-    #[task(binds = EIC)]
-    fn eic(_: eic::Context) {}
+    #[task(binds = SERCOM0, resources = [transport, uart, sercom0])]
+    fn sercom0(cx: sercom0::Context) {
+        if cx
+            .resources
+            .sercom0
+            .usart_mut()
+            .intflag
+            .read()
+            .rxc()
+            .bit_is_set()
+        {
+            let data = cx.resources.sercom0.usart().data.read().bits();
+            cx.resources
+                .transport
+                .ingest(data)
+                .map_or((), |resp| cx.resources.uart.bwrite_all(&resp).unwrap());
+        }
+    }
 };
